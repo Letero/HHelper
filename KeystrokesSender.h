d81d6b98e57884108be508390911247b874e391d
unknown <jkurzynski93@gmail.com>
2020-04-01 00:41:30 +0200

    reading user input, fixed but with multiple chars in a row

---
+++ KeystrokesSender.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QObject>
+#include <QDebug>
+#include <Windows.h>
+//#include <WinUser.h>
+
+class KeystrokesSender : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+private:
+    QString mText;
+    QString targetWindow;
+    void SendKey(BYTE virtualKey);
+    void SendKeyUppercase(BYTE virtualKey);
+    void SendMessage(QString message);
+
+public:
+    QString text() const{
+        return mText;
+    }
+    void setText(const QString &text){
+         if(text == mText)
+             return;
+         mText = text;
+         emit textChanged(mText);
+     }
+    explicit KeystrokesSender(QObject *parent = nullptr);
+    Q_INVOKABLE void setupTargetWindow(QString target);
+    Q_INVOKABLE void sendKeystroke( const QString &message);
+
+signals:
+    void textChanged(const QString & text);
+};
+
