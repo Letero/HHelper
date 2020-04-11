ce6f0f5b76c44516cf4442984c99ca850882eded
unknown <jkurzynski93@gmail.com>
2020-04-11 19:02:05 +0200

    somehow working config

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -1,32 +1,16 @@
 #include "JsonParser.h"
 
-JsonParser::JsonParser(QObject *parent) : QObject(parent), settings(readFile())
+JsonParser::JsonParser(QObject *parent) : QObject(parent)
 {
-
+    loadConfig("config.json");
 }
 
-QString JsonParser::getTargetWindowName()
+bool JsonParser::loadConfig(QString filename)
 {
-    QJsonObject main_settings = this->settings.value(QString("main_settings")).toObject();
-    QJsonValue targetWindow = main_settings.value(QString("target_window"));
-    return targetWindow.toString();
-}
-
-void JsonParser::setTargetWindowName(QString windowName)
-{
-    qDebug() << windowName;
-}
-
-void JsonParser::writeToFile()
-{
-}
-
-QJsonObject JsonParser::readFile()
-{
-    QFile file("config.json");
+    QFile file(filename);
     if (!file.open(QIODevice::ReadOnly)) {
         qDebug() << "file error";
-//        return;
+        return false;
 //        todo: throw exception
     }
     const QByteArray ba = file.readAll();
@@ -34,12 +18,46 @@ QJsonObject JsonParser::readFile()
 
     QJsonParseError err;
     QJsonDocument doc = QJsonDocument::fromJson(ba, &err);
+    qDebug() << err.errorString();
+    qDebug() << err.offset;
 
-//    qDebug() << err.errorString();
-//    qDebug() << err.offset;
+    m_config = doc.object();
 
-    QJsonObject sett2 = doc.object();
-//    qDebug() << sett2.isEmpty();
+    if (!m_config.isEmpty())
+    {
+        m_mainSettings = m_config.value(QString("main_settings")).toObject();
+    } else
+    {
+        return false;
+    }
+//    saveConfig("config.json");
+    return true;
+}
 
-    return sett2;
+bool JsonParser::saveConfig(QString filename)
+{
+    QFile saveFile(filename);
+    if (!saveFile.open(QIODevice::WriteOnly)) {
+        qDebug() << "file error";
+        return false;
+//        todo: throw exception
+    }
+    m_config["main_settings"] = m_mainSettings;
+    QJsonDocument saveDoc(m_config);
+    saveFile.write(saveDoc.toJson());
+
+    return true;
+}
+
+QString JsonParser::getTargetWindowName()
+{
+    QJsonValue targetWindow = m_mainSettings.value(QString("target_window"));
+    return targetWindow.toString();
+}
+
+void JsonParser::setTargetWindowName(QString windowName)
+{
+    QJsonValue targetWindow = m_mainSettings.value(QString("target_window"));
+    m_mainSettings["target_window"] = windowName;
+    saveConfig("config.json");
 }
