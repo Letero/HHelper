4fbe3bad736aa534093a24a7399e1a2ccf5cc31d
unknown <jkurzynski93@gmail.com>
2020-04-21 01:31:36 +0200

    added controller

--- Controller.cpp
+++ Controller.cpp
@@ -1,6 +1,42 @@
 #include "Controller.h"
 
-Controller::Controller()
+
+Controller::Controller(QObject *parent) : QObject(parent), m_buttonModel(new ButtonModel)
 {
 
+}
+
+void Controller::init()
+{
+    m_buttonModel->init(m_jsonParser.getButtonsData());
+}
+
+ButtonModel *Controller::getButtonModel() const
+{
+    return m_buttonModel.get();
+}
+
+QString Controller::getTargetWindow()
+{
+    return m_jsonParser.getTargetWindowName();
+}
+
+void Controller::setTargetWindow(QString name)
+{
+    m_jsonParser.setTargetWindowName(name);
+}
+
+void Controller::saveCurrentConfig()
+{
+    m_jsonParser.saveCurrentConfig();
+}
+
+void Controller::setSlotName(QString name)
+{
+    m_jsonParser.setSlotName(name);
+}
+
+QString Controller::getSlotName()
+{
+    return m_jsonParser.getSlotName();
 }
