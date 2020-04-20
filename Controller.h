87638bd6377f5cd659c566ee27036ad33d61c9fc
unknown <jkurzynski93@gmail.com>
2020-04-21 01:31:36 +0200

    added controller

--- Controller.h
+++ Controller.h
@@ -1,10 +1,25 @@
 #pragma once
 
 #include <QObject>
+#include "ButtonModel.h"
+#include "JsonParser.h"
 
-class Controller
+class Controller : public QObject
 {
+    Q_OBJECT
+    Q_PROPERTY(ButtonModel *buttonModel READ getButtonModel CONSTANT)
 public:
-    Controller();
+    Controller(QObject *parent = nullptr);
+    Q_INVOKABLE void init();
+    ButtonModel *getButtonModel() const;
+    Q_INVOKABLE QString getTargetWindow();
+    Q_INVOKABLE void setTargetWindow(QString name);
+    Q_INVOKABLE void saveCurrentConfig();
+    Q_INVOKABLE void setSlotName(QString name);
+    Q_INVOKABLE QString getSlotName();
+
+private:
+    QScopedPointer<ButtonModel> m_buttonModel;
+    JsonParser m_jsonParser;
 };
 
