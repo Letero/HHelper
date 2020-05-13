9141146ce1e64f79b88db2dfc871ad7ac5f43147
unknown <jkurzynski93@gmail.com>
2020-05-13 19:38:44 +0200

    Added devMode QPROPERTY

--- KeystrokesSender.h
+++ KeystrokesSender.h
@@ -9,31 +9,27 @@ class KeystrokesSender : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+    Q_PROPERTY(bool devMode READ getDevMode WRITE setDevMode NOTIFY devModeChanged)
 private:
     QString mText;
     QString targetWindow;
     void sendKey(BYTE virtualKey);
     void sendKeyUppercase(const BYTE &virtualKey);
     void sendMessage(const QString &message);
+    bool mDevMode;
 
 public:
-    QString text() const
-    {
-        return mText;
-    }
-    void setText(const QString &text)
-    {
-        if (text == mText) {
-            return;
-        }
-        mText = text;
-        emit textChanged(mText);
-    }
+    QString text() const;
+    void setText(const QString &text);
+    bool getDevMode() const;
+    void setDevMode(bool devMode);
     explicit KeystrokesSender(QObject *parent = nullptr);
     Q_INVOKABLE void setupTargetWindow(const QString &target);
     Q_INVOKABLE void sendKeystroke(const QStringList &messages);
 
+
 signals:
     void textChanged(const QString &text);
+    void devModeChanged(bool devMode);
 };
 
